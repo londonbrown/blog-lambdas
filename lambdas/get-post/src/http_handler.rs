4118f412dec45173edd46c9d99ae2cc20a7cab83
londonbrown <londonchaim@gmail.com>
2025-03-09 20:51:56 -0700

    test: Log dynamodb response for post-001 in get-post lambda

--- lambdas/get-post/src/http_handler.rs
+++ lambdas/get-post/src/http_handler.rs
@@ -1,8 +1,27 @@
-use lambda_http::{Body, Error, Request, Response};
-use serde_json::{json};
+use lambda_http::{tracing, Body, Request, Response};
+use aws_sdk_dynamodb::Client;
+use aws_sdk_dynamodb::types::AttributeValue;
+use std::env;
+use tracing::info;
 
-pub(crate) async fn function_handler(_event: Request) -> Result<Response<Body>, Error> {
+pub(crate) async fn function_handler(_event: Request) -> Result<Response<Body>, Box<dyn std::error::Error + Send + Sync>> {
+    let client = Client::new(&aws_config::load_from_env().await);
+    let table_name = env::var("BLOG_POSTS_TABLE").expect("BLOG_POSTS_TABLE not set");
+
+    // TODO remove hardcoded post and obtain from event context
+    let partition_key = "POST#post-001";
+
+    // Query all items under partition_key
+    let result = client.query()
+        .table_name(&table_name)
+        .key_condition_expression("PK = :pk")
+        .expression_attribute_values(":pk", AttributeValue::S(partition_key.to_string()))
+        .send()
+        .await?;
+
+    info!("DynamoDB response: {:?}", result);
+
     Ok(Response::builder()
-        .status(404)
-        .body(json!({"error": "Post not found"}).to_string().into())?)
+        .status(200)
+        .body("Query executed successfully. Check the logs for item details".into())?)
 }
