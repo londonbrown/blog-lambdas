7265bbaf754080c1ad117d762e533426b24a7e46
londonbrown <londonchaim@gmail.com>
2025-03-10 00:27:12 -0700

    add: implement get-post handler

--- lambdas/get-post/src/http_handler.rs
+++ lambdas/get-post/src/http_handler.rs
@@ -1,9 +1,10 @@
-use lambda_http::{Body, Request, Response};
+use lambda_http::{Body, Request, RequestExt, Response};
 use aws_sdk_dynamodb::Client;
 use aws_sdk_dynamodb::types::AttributeValue;
 use serde::{Deserialize, Serialize};
 use std::env;
-use serde_dynamo::{Item};
+use serde_dynamo::from_item;
+use serde_json::json;
 use tracing::info;
 
 // TODO make this struct a shared object
@@ -38,12 +39,13 @@ struct Comment {
     created_at: String
 }
 
-pub(crate) async fn function_handler(_event: Request) -> Result<Response<Body>, Box<dyn std::error::Error + Send + Sync>> {
+pub(crate) async fn function_handler(event: Request) -> Result<Response<Body>, Box<dyn std::error::Error + Send + Sync>> {
     let client = Client::new(&aws_config::load_from_env().await);
     let table_name = env::var("BLOG_POSTS_TABLE").expect("BLOG_POSTS_TABLE not set");
 
-    // TODO remove hardcoded post and obtain from event context
-    let partition_key = "POST#post-001";
+    let path_parameters = event.path_parameters();
+    let post_id = path_parameters.first("id").unwrap_or_default();
+    let partition_key = format!("POST#{}", post_id);
 
     // Query all items under partition_key
     let result = client.query()
@@ -53,15 +55,32 @@ pub(crate) async fn function_handler(_event: Request) -> Result<Response<Body>, 
         .send()
         .await?;
 
-    info!("DynamoDB response: {:?}", result);
+    let mut meta: Option<BlogPost> = None;
+    let mut comments: Vec<Comment> = Vec::new();
 
     for raw_item in result.items.unwrap_or_default() {
-        info!("raw_item: {:?}", raw_item);
-        let item: Item = raw_item.into();
-        info!("item: {:?}", item);
+        if let Some(sk) = raw_item.get("SK").and_then(|v| v.as_s().ok()) {
+            if sk == "META" {
+                match from_item(raw_item) {
+                    Ok(post) => meta = Some(post),
+                    Err(e) => info!("Failed to deserialize BlogPost: {:?}", e),
+                }
+            } else if sk.starts_with("COMMENT#") {
+                match from_item(raw_item) {
+                    Ok(comment) => comments.push(comment),
+                    Err(e) => info!("Failed to deserialize Comment: {:?}", e),
+                }
+            }
+        }
     }
+
+    let response_body = serde_json::to_string(&json!({
+        "meta": meta,
+        "comments": comments
+    }))?;
 
     Ok(Response::builder()
         .status(200)
-        .body("Query executed successfully. Check the logs for item details".into())?)
+        .header("Content-Type", "application/json")
+        .body(response_body.into())?)
 }
