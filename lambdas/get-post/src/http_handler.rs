66c1d9dc013dcbbb18381ff1e6616b93c759aa71
londonbrown <londonchaim@gmail.com>
2025-03-09 21:47:14 -0700

    test: Log found posts and comments

--- lambdas/get-post/src/http_handler.rs
+++ lambdas/get-post/src/http_handler.rs
@@ -1,8 +1,41 @@
-use lambda_http::{tracing, Body, Request, Response};
+use lambda_http::{Body, Request, Response};
 use aws_sdk_dynamodb::Client;
 use aws_sdk_dynamodb::types::AttributeValue;
+use serde::{Deserialize, Serialize};
 use std::env;
 use tracing::info;
+
+// TODO make this struct a shared object
+#[derive(Debug, Deserialize, Serialize)]
+struct BlogPost {
+    #[serde(rename = "PK")]
+    pk: String,
+    #[serde(rename = "SK")]
+    sk: String,
+    title: String,
+    #[serde(rename = "authorId")]
+    author_id: String,
+    tags: Option<Vec<String>>,
+    published: Option<String>,
+    #[serde(rename = "createdAt")]
+    created_at: String,
+    #[serde(rename = "contentKey")]
+    content_key: String
+}
+
+// TODO make this struct a shared object
+#[derive(Debug, Deserialize, Serialize)]
+struct Comment {
+    #[serde(rename = "PK")]
+    pk: String,
+    #[serde(rename = "SK")]
+    sk: String,
+    #[serde(rename = "userId")]
+    user_id: String,
+    text: String,
+    #[serde(rename = "createdAt")]
+    created_at: String
+}
 
 pub(crate) async fn function_handler(_event: Request) -> Result<Response<Body>, Box<dyn std::error::Error + Send + Sync>> {
     let client = Client::new(&aws_config::load_from_env().await);
@@ -20,6 +53,16 @@ pub(crate) async fn function_handler(_event: Request) -> Result<Response<Body>, 
         .await?;
 
     info!("DynamoDB response: {:?}", result);
+
+    for item in result.items.unwrap_or_default() {
+        if let Some(sk) = item.get("SK").and_then(|v| v.as_s().ok()) {
+            if sk == "META" {
+                info!("Found META item: {:?}", item)
+            } else if sk.starts_with("COMMENT#") {
+                info!("Found COMMENT item: {:?}", item)
+            }
+        }
+    }
 
     Ok(Response::builder()
         .status(200)
