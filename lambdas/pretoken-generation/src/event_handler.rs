211ab3f7b3141cf87ca7720ccd4f31d4aa67be70
londonbrown <londonchaim@gmail.com>
2025-03-16 11:57:49 -0700

    change: scope of user groups, also suppress unrecognized scopes

--- lambdas/pretoken-generation/src/event_handler.rs
+++ lambdas/pretoken-generation/src/event_handler.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::env;
 use aws_lambda_events::cognito::{ClaimsAndScopeOverrideDetailsV2, CognitoAccessTokenGenerationV2, CognitoEventUserPoolsPreTokenGenResponseV2, CognitoEventUserPoolsPreTokenGenV2};
 use lambda_runtime::LambdaEvent;
@@ -15,43 +15,44 @@ pub(crate) async fn function_handler(event: LambdaEvent<CognitoEventUserPoolsPre
 
     let group_to_scope_map: HashMap<&str, Vec<String>> = HashMap::from([
         ("admin", vec![
-            format!("{}/post.read", api_blog_domain_url),
-            format!("{}/post.write", api_blog_domain_url),
-            format!("{}/post.delete", api_blog_domain_url),
-            format!("{}/comment.read", api_blog_domain_url),
-            format!("{}/comment.write", api_blog_domain_url),
-            format!("{}/comment.delete", api_blog_domain_url),
+            format!("{}/admin.read", api_blog_domain_url),
+            format!("{}/admin.write", api_blog_domain_url),
+            format!("{}/admin.delete", api_blog_domain_url)
         ]),
         ("author", vec![
-            format!("{}/post.read", api_blog_domain_url),
-            format!("{}/post.write", api_blog_domain_url),
-            format!("{}/comment.read", api_blog_domain_url),
-            format!("{}/comment.write", api_blog_domain_url),
-            format!("{}/comment.delete", api_blog_domain_url),
+            format!("{}/author.read", api_blog_domain_url),
+            format!("{}/author.write", api_blog_domain_url),
+            format!("{}/author.delete", api_blog_domain_url)
         ]),
         ("commenter", vec![
-            format!("{}/comment.read", api_blog_domain_url),
-            format!("{}/comment.write", api_blog_domain_url),
-            format!("{}/comment.delete", api_blog_domain_url),
+            format!("{}/commenter.read", api_blog_domain_url),
+            format!("{}/commenter.write", api_blog_domain_url),
+            format!("{}/commenter.delete", api_blog_domain_url)
         ]),
         ("guest", vec![
-            format!("{}/post.read", api_blog_domain_url),
-            format!("{}/comment.read", api_blog_domain_url),
+            format!("{}/guest.read", api_blog_domain_url),
+            format!("{}/guest.write", api_blog_domain_url),
+            format!("{}/guest.delete", api_blog_domain_url)
         ]),
     ]);
 
-    let mut assigned_scopes = Vec::new();
+    let mut assigned_scopes: HashSet<String> = HashSet::new();
+    assigned_scopes.insert("aws.cognito.signin.user.admin".to_string());
 
     for group in request.group_configuration.groups_to_override.clone() {
         if let Some(scopes) = group_to_scope_map.get(group.as_str()) {
-            assigned_scopes.extend_from_slice(scopes);
+            assigned_scopes.extend(scopes.iter().cloned())
         }
     }
+
+    let requested_scopes: HashSet<String> = request.scopes.iter().cloned().collect();
+    let suppressed_scopes: Vec<String> = requested_scopes.difference(&assigned_scopes).cloned().collect();
 
     let response = CognitoEventUserPoolsPreTokenGenResponseV2 {
         claims_and_scope_override_details: Some(ClaimsAndScopeOverrideDetailsV2 {
             access_token_generation: Some(CognitoAccessTokenGenerationV2 {
-                scopes_to_add: assigned_scopes,
+                scopes_to_add: assigned_scopes.into_iter().collect(),
+                scopes_to_suppress: suppressed_scopes,
                 ..Default::default()
             }),
             ..Default::default()
