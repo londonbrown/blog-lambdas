733123f6ff90736370cd594b891faabe7af921a6
londonbrown <londonchaim@gmail.com>
2025-03-18 21:48:51 -0700

    change: use lambda event API Gateway struct in delete-post, remove lambda_http dependency from project

--- lambdas/delete-post/src/http_handler.rs
+++ lambdas/delete-post/src/http_handler.rs
@@ -1,13 +1,13 @@
-use lambda_http::{Body, Request, RequestExt, Response};
+use aws_lambda_events::apigw::{ApiGatewayProxyRequest, ApiGatewayProxyResponse};
+use aws_lambda_events::encodings::Body;
+use lambda_runtime::LambdaEvent;
 use tracing::log::info;
 
 pub(crate) async fn function_handler(
-    event: Request,
-) -> Result<Response<Body>, Box<dyn std::error::Error + Send + Sync>> {
-    let request_context = event.request_context();
-    let authorizer = request_context
-        .authorizer()
-        .ok_or("Missing authorizer context")?;
+    event: LambdaEvent<ApiGatewayProxyRequest>,
+) -> Result<ApiGatewayProxyResponse, Box<dyn std::error::Error + Send + Sync>> {
+    let request_context = event.payload.request_context;
+    let authorizer = request_context.authorizer;
 
     info!("Authorizer: {:?}", authorizer);
 
@@ -22,8 +22,9 @@ pub(crate) async fn function_handler(
 
     // TODO extract group id from claims
 
-    Ok(Response::builder()
-        .status(200)
-        .body("Hello, World!".into())
-        .unwrap())
+    Ok(ApiGatewayProxyResponse {
+        status_code: 200,
+        body: Some(Body::Text("Hello, world!".to_string())),
+        ..Default::default()
+    })
 }
