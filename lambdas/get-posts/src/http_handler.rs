e6681fe6908a53dce305d4e7c660d78751c97b9e
londonbrown <londonchaim@gmail.com>
2025-03-18 21:36:08 -0700

    change: use lambda event API Gateway struct in get-posts

--- lambdas/get-posts/src/http_handler.rs
+++ lambdas/get-posts/src/http_handler.rs
@@ -1,17 +1,25 @@
+use aws_lambda_events::apigw::{ApiGatewayProxyRequest, ApiGatewayProxyResponse};
+use aws_lambda_events::encodings::Body;
+use aws_lambda_events::http::header::CONTENT_TYPE;
+use aws_lambda_events::http::HeaderMap;
 use aws_sdk_dynamodb::Client;
-use lambda_http::{Body, Request, RequestExt, Response};
+use lambda_runtime::LambdaEvent;
 use serde_json::json;
 use shared::db::fetch_published_posts;
 use shared::errors::ApiErrorResponse;
 use std::env;
 
 pub(crate) async fn function_handler(
-    event: Request,
-) -> Result<Response<Body>, Box<dyn std::error::Error + Send + Sync>> {
+    event: LambdaEvent<ApiGatewayProxyRequest>,
+) -> Result<ApiGatewayProxyResponse, Box<dyn std::error::Error + Send + Sync>> {
     let client = Client::new(&aws_config::load_from_env().await);
     let table_name = env::var("BLOG_POSTS_TABLE").expect("BLOG_POSTS_TABLE not set");
 
-    let query_string_parameters = event.query_string_parameters();
+    let query_string_parameters = event.payload.query_string_parameters;
+
+    let mut header_map = HeaderMap::new();
+    header_map.insert(CONTENT_TYPE, "application/json".parse().unwrap());
+
     let limit = query_string_parameters
         .first("limit")
         .and_then(|l| l.parse().ok());
@@ -22,9 +30,13 @@ pub(crate) async fn function_handler(
     let (posts, next_token) = fetch_published_posts(&client, &table_name, limit, next_token).await;
 
     if posts.is_empty() {
-        return Ok(Response::builder()
-            .status(404)
-            .body(serde_json::to_string(&ApiErrorResponse::new("No posts found"))?.into())?);
+        let error = serde_json::to_string(&ApiErrorResponse::new("No posts found"))?;
+        return Ok(ApiGatewayProxyResponse {
+            status_code: 404,
+            headers: header_map,
+            body: Some(Body::Text(error)),
+            ..Default::default()
+        });
     }
 
     let response_body = json!({
@@ -32,8 +44,10 @@ pub(crate) async fn function_handler(
         "nextToken": next_token
     });
 
-    Ok(Response::builder()
-        .status(200)
-        .header("Content-Type", "application/json")
-        .body(serde_json::to_string(&response_body)?.into())?)
+    Ok(ApiGatewayProxyResponse {
+        status_code: 200,
+        headers: header_map,
+        body: Some(Body::Text(response_body.to_string())),
+        ..Default::default()
+    })
 }
